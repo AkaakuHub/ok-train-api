663825c9daa41549d1f4e90b8a394393d95e4264
AkaakuHub <105225777+github-actions@users.noreply.github.com>
2025-04-24 10:35:08 +0900

    chore: asset直接取得APIを復活

--- src/assets/assets.controller.ts
+++ src/assets/assets.controller.ts
@@ -1,9 +1,26 @@
+import { Response } from 'express';
 import {
   Controller,
+  Get,
+  Param,
+  Res,
+  HttpException,
+  HttpStatus,
 } from "@nestjs/common";
 import { AssetsService } from "./assets.service";
 
 @Controller("assets")
 export class AssetsController {
-  constructor(private readonly assetsService: AssetsService) {}
+  constructor(private readonly assetsService: AssetsService) { }
+
+  @Get(':filename')
+  async getAssetJson(@Param('filename') filename: string, @Res() res: Response) {
+    try {
+      const data = await this.assetsService.getJson(filename);
+      res.setHeader('Content-Type', 'application/json; charset=utf-8');
+      res.send(data);
+    } catch (e) {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
+  }
 }
